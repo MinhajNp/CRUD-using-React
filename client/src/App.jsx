8aca9297c926c3ebe2ded0e1c970ed68601a5273
MinhajNp <minhajmohammednp@gmail.com>
2025-01-28 19:25:58 +0530

    updated profile and home private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,15 @@ import Home from "./pages/Home"
 import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
+import PrivateRoute from "./components/PrivateRoute"
 
 function App() {
   return ( <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/profile" element={<Profile/>}/>
+      <Route element={<PrivateRoute/>}>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/" element={<Home/>}/>
+      </Route>
       <Route path="/sign-in" element={<SignIn/>}/>
       <Route path="/sign-up" element={<SignUp/>}/>
     </Routes>
